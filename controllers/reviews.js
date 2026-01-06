096e163f71b7dc548c5cb5615c30a5fabf7c250b
Mehwash1505 <asmanasreen388@gmail.com>
2026-01-06 23:57:53 +0530

    Update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,7 +6,7 @@ module.exports.createReview = async(req, res) => {
     let newReview = new Review(req.body.review);
     newReview.author = req.user._id;
     console.log(newReview);
-    listing.reviews.push(newReview);
+    listing.reviews.push(newReview); 
 
     await newReview.save();
     await listing.save();        // console.log("new review saved");
